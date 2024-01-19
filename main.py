0f4e8575940c84b08cc81b7d1ce2a5809c8e9548
ekarasz <edit.karasz@gmail.com>
2024-01-19 19:59:26 +0100

    Update main.py
    Replaced the default file with the code.

--- main.py
+++ main.py
@@ -2,15 +2,131 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import configparser
+import logging
+import pandas as pd
+import random
+# import requests
+# from io import StringIO
+import os
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# def load_file(url):
+#    file_id = url.split('/')[-2]
+#    google_drive_url = 'https://drive.google.com/uc?export=download&id=' + file_id
+#    url2 = requests.get(google_drive_url).text
+#    csv_raw = StringIO(url2)
+#    df = pd.read_csv(csv_raw)
+#    return df
+
+
+def choose_file(exercises_folder):
+
+    # noinspection PyBroadException
+    try:
+        list_of_files = os.listdir(exercises_folder)
+    except Exception:
+        logging.error(f'Cannot open Exercises directory')
+        sys.exit()
+
+    logging.debug(f'List of files in Exercises folder: {list_of_files}')
+    file_names = []
+    for input_file in list_of_files:
+        file_parts = input_file.split('.')
+        if file_parts[1] == 'csv':
+            file_names.append(file_parts[0])
+    return file_names
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # url = 'https://drive.google.com/file/d/1FZkEvVHDOQISbQn28Yw8OcWT3sA61RTd/view?usp=drive_link'
+    # df = load_file(url)
+
+    # Variables from the config file
+    config = configparser.ConfigParser()
+
+    # noinspection PyBroadException
+    try:
+        config.read('WordPractice.conf')
+    except Exception:
+        logging.error(f'Unable to process the config file')
+        sys.exit()
+
+    directory = config.get('word_practice', 'directory')
+    subjects = choose_file(directory)
+
+    if len(subjects) == 0:
+        logging.error(f'There is no exercise file in the Exercises folder')
+        sys.exit()
+    elif len(subjects) == 1:
+        subject = subjects[0]
+    else:
+        subject = input(f'Choose a subject: {subjects} ')
+
+    exercise = directory + subject + '.csv'
+
+    # noinspection PyBroadException
+    try:
+        df = pd.read_csv(exercise)
+        logging.debug(df.head())
+    except Exception:
+        logging.error(f'Unable to process the exercise file: {exercise} ')
+        sys.exit()
+
+    # the number of words but the dataframe indexing from 0
+    word_count = len(df.index)
+    logging.debug(f'Number of words in file: {word_count}')
+
+    number_of_tries = int(input(f'Number of tries? '))
+
+    repeat = True
+    while repeat:
+        header = df.columns.to_list()
+        from_language = header[1]
+        to_language = header[0]
+        right_answers = {}
+        wrong_answers = {}
+        was_once = []
+
+        for i in range(number_of_tries):
+            # Random word from row 0 to row len(df.index)
+
+            index = random.randint(0, word_count - 1)
+            while index in was_once:
+                index = random.randint(0, word_count - 1)
+            was_once.append(index)
+
+            # from language to language variable
+            original_word = df.loc[index, from_language]
+            original_word = original_word.strip()
+            original_word = original_word.lower()
+
+            reply_word = input(f'Translate {original_word}: ')
+            reply_word = reply_word.strip()
+            reply_word = reply_word.lower()
+
+            to_word = df.loc[index, to_language]
+            to_word = to_word.strip()
+            to_word = to_word.lower()
+
+            if to_word == reply_word:
+                print(f'Correct')
+                right_answers[original_word] = to_word
+            else:
+                print(f'Incorrect')
+                print(f'{original_word} is {to_word}')
+                wrong_answers[original_word] = to_word
+
+        percentage = len(right_answers) * 100 / number_of_tries
+
+        print()
+        print(f'*** Summary ***')
+        print(f'You got {percentage}% right')
+        print(f'Wrong answers: {wrong_answers}')
+        print(f'Right answers: {right_answers}')
+        again = input('Do you want to give it another try? [Y/N] (Default: y): ')
+        if 'n' in again.lower():
+            repeat = False
