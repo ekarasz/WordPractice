92618b4f59b3c2f2e78fc0efcda59380262f1f0b
Edit Karasz <edit.karasz@himinds.expert>
2024-03-10 08:26:06 +0100

    input checks and separate class file

--- main.py
+++ main.py
@@ -4,72 +4,7 @@ import logging
 import random
 import os
 import sys
-
-
-class DictionaryWord:
-    report = []
-
-    def __init__(self, from_word: str, to_word: str, in_word=''):
-        # validation
-        assert from_word not in (None, ''), f"The word we translate from must be defined!"
-        assert to_word not in (None, ''), f"The word we translate to must be defined!"
-
-        # initialise the objects
-        self.from_word = from_word
-        self.to_word = to_word
-        self.in_word = in_word
-        self.counter = 0
-        self.right_answer_counter = 0
-
-        # populate the dictionary with the exercises
-        DictionaryWord.report.append(self)
-
-    @classmethod
-    def instantiate_from_csv(cls, words, from_language, to_language):
-        if len(words) > 0:
-            for word in words:
-                logging.debug(f'word: {word}')
-                logging.debug(f'from_language: .{from_language}.')
-                logging.debug(f'from_language: .{to_language}.')
-                logging.debug(f'from word: .{word.get(from_language)}.')
-                logging.debug(f'to word: .{word.get(to_language)}.')
-                DictionaryWord(
-                    from_word=word.get(from_language),
-                    to_word=word.get(to_language),
-                )
-        else:
-            logging.error(f'Dictionary file is empty!')
-            sys.exit()
-
-    def __repr__(self):
-        return self.show_word()
-
-    def show_word(self):
-        return f"{self.from_word} is {self.to_word}"
-
-    def add_answer(self, reply_word):
-        self.in_word = reply_word
-
-    def increment_counter(self, from_word):
-        if self.from_word == from_word:
-            self.counter += self.counter
-    def was_once(self):
-        if self.counter >= 0:
-            return True
-        else:
-            return False
-
-    def right_answer(self):
-        logging.debug(f'to word {self.to_word}')
-        logging.debug(f'in word {self.in_word}')
-        if self.to_word == self.in_word:
-            return True
-        else:
-            return False
-
-    # add a window for acceptance of type of errors
-    # def AcceptableMistake(self):
-
+from dictionaryword import DictionaryWord
 def choose_file(exercises_folder):
     try:
         list_of_files = os.listdir(exercises_folder)
@@ -98,7 +33,7 @@ if __name__ == '__main__':
         logging.error(f'Unable to process the config file!')
         sys.exit()
 
-
+    number_of_tries = int(config.get('word_practice', 'number_of_tries'))
     directory = config.get('word_practice', 'directory')
     subjects = choose_file(directory)
 
@@ -137,7 +72,8 @@ if __name__ == '__main__':
     word_count = len(DictionaryWord.report)
     logging.debug(f'Number of words: {word_count}')
 
-    number_of_tries = int(input(f'Number of tries: '))
+    while number_of_tries <= 0:
+        number_of_tries = int(input(f'Number of tries: '))
 
     repeat = True
     while repeat:
